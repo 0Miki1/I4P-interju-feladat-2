2f8dfbb9e477e11c6179147a3b462d9385bce1f0
Ábel Miklós <abelmiklos@outlook.hu>
2023-07-28 14:44:12 +0200

    Algorithm for key guessing

--- I4P-interju-feladat-2/Program.cs
+++ I4P-interju-feladat-2/Program.cs
@@ -10,6 +10,30 @@ namespace I4P_interju_feladat_2
     {
         static void Main(string[] args)
         {
+            string msg1 = "qxpv";
+            string msg2 = "rwwv";
+
+            char c = 'a';
+            //int charCnt = 'a';
+
+            string key = string.Empty;
+
+            for (int i = 0; i < msg1.Length; i++)
+            {
+                while ((msg1[i] + c) != (msg2[i] + c))
+                {
+                    //charCnt++;
+                    c++;
+                }
+
+                //key += ((char)charCnt);
+                key += c;
+                c = 'a';
+            }
+
+            Console.WriteLine(key);
+
+            //Console.ReadKey();
         }
     }
 }

1cfc69d350b49035bb00af19849b053ae307a187
Ábel Miklós <abelmiklos@outlook.hu>
2023-08-06 17:45:54 +0200

    PossibleWords class added and implemented

---
+++ I4P-interju-feladat-2/PossibleWords.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace I4P_interju_feladat_2
+{
+    internal class PossibleWords
+    {
+        private readonly List<string> words;
+
+        public PossibleWords(string filePath)
+        {
+            words = FileRead(filePath);
+        }
+
+        public List<string> Words { get => words; }
+
+        private List<string> FileRead(string filePath)
+        {
+            FileStream fs = new FileStream(@"..\..\" + filePath, FileMode.Open);
+            StreamReader sr = new StreamReader(fs, Encoding.Default);
+            List<string> temp = new List<string>();
+
+            while (!sr.EndOfStream)
+            {
+                temp.Add(sr.ReadLine());
+            }
+
+            sr.Close();
+            fs.Close();
+
+            return temp;
+        }
+    }
+}
--- I4P-interju-feladat-2/Program.cs
+++ I4P-interju-feladat-2/Program.cs
@@ -1,39 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace I4P_interju_feladat_2
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            string msg1 = "qxpv";
-            string msg2 = "rwwv";
+        { 
 
-            char c = 'a';
-            //int charCnt = 'a';
-
-            string key = string.Empty;
-
-            for (int i = 0; i < msg1.Length; i++)
-            {
-                while ((msg1[i] + c) != (msg2[i] + c))
-                {
-                    //charCnt++;
-                    c++;
-                }
-
-                //key += ((char)charCnt);
-                key += c;
-                c = 'a';
-            }
-
-            Console.WriteLine(key);
-
-            //Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
